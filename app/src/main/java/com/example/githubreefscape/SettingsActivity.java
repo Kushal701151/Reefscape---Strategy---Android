72c710ed7156cdea999198b41ddd14d7cddc5eb5
kreeshap <kreesha.patel0831@gmail.com>
2025-01-28 19:47:56 -0500

    Put in Methods for Java and onClick

--- app/src/main/java/com/example/githubreefscape/SettingsActivity.java
+++ app/src/main/java/com/example/githubreefscape/SettingsActivity.java
@@ -2,20 +2,13 @@ package com.example.githubreefscape;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.TextView;
 import android.view.View;
 import android.util.DisplayMetrics;
-import android.content.Intent;
 
 public class SettingsActivity extends Activity {
-
 
     public CheckBox autoDisable;
     public int autoDisableInt = 1;
@@ -42,20 +35,18 @@ public class SettingsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        int screenwidth = displayMetrics.widthPixels;
-        int screenHeight = displayMetrics.heightPixels;
 
+        autoDisable = findViewById(R.id.autoDisableID);
+        teleDisable = findViewById(R.id.teleDisableID);
+        endgameDisable = findViewById(R.id.endgameDisableID);
+        coralDisable = findViewById(R.id.coralDisableID);
+        algaeDisable = findViewById(R.id.algaeDisableID);
+        parkDisable = findViewById(R.id.parkDisableID);
+        databaseEnable = findViewById(R.id.databaseEnableID);
+        hpDisable = findViewById(R.id.hpDisableID);
 
-        autoDisable = (CheckBox) findViewById(R.id.autoDisableID);
-        teleDisable = (CheckBox) findViewById(R.id.teleDisableID);
-        endgameDisable = (CheckBox) findViewById(R.id.endgameDisableID);
-        coralDisable = (CheckBox) findViewById(R.id.coralDisableID);
-        algaeDisable = (CheckBox) findViewById(R.id.algaeDisableID);
-        parkDisable = (CheckBox) findViewById(R.id.parkDisableID);
-        databaseEnable = (CheckBox) findViewById(R.id.databaseEnableID);
-        hpDisable = (CheckBox) findViewById(R.id.hpDisableID);
+        colorLight = findViewById(R.id.colorLightID);
+        colorDark = findViewById(R.id.colorDarkID);
 
         autoDisable.setOnClickListener(this::toggleAutoDisable);
         teleDisable.setOnClickListener(this::toggleTeleDisable);
@@ -69,39 +60,92 @@ public class SettingsActivity extends Activity {
         colorDark.setOnClickListener(this::toggleColorRadio);
     }
 
+
     public void toggleAutoDisable(View view) {
         autoDisableInt = autoDisable.isChecked() ? 1 : 0;
+        disableAutoFeatures(!autoDisable.isChecked());
     }
 
     public void toggleTeleDisable(View view) {
         teleDisableInt = teleDisable.isChecked() ? 1 : 0;
+        disableTeleFeatures(!teleDisable.isChecked());
     }
 
     public void toggleEndgameDisable(View view) {
         endgameDisableInt = endgameDisable.isChecked() ? 1 : 0;
+        disableEndgameFeatures(!endgameDisable.isChecked());
     }
 
     public void toggleCoralDisable(View view) {
         coralDisableInt = coralDisable.isChecked() ? 1 : 0;
+        disableCoralFeatures(!coralDisable.isChecked());
     }
 
     public void toggleAlgaeDisable(View view) {
         algaeDisableInt = algaeDisable.isChecked() ? 1 : 0;
+        disableAlgaeFeatures(!algaeDisable.isChecked());
     }
 
     public void toggleParkDisable(View view) {
         parkDisableInt = parkDisable.isChecked() ? 1 : 0;
+        disableParkFeatures(!parkDisable.isChecked());
     }
 
     public void toggleHpDisable(View view) {
         hpDisableInt = hpDisable.isChecked() ? 1 : 0;
+        disableHpFeatures(!hpDisable.isChecked());
     }
 
     public void toggleDatabaseEnable(View view) {
         databaseEnableInt = databaseEnable.isChecked() ? 1 : 0;
+        disableDatabaseFeatures(!databaseEnable.isChecked());
     }
 
     public void toggleColorRadio(View view) {
         colorRadio = colorLight.isChecked() ? 0 : 1;
+        updateColorScheme();
+    }
+
+    public void disableAutoFeatures(boolean disable) {
+    }
+
+    public void disableTeleFeatures(boolean disable) {
+    }
+
+    public void disableEndgameFeatures(boolean disable) {
+    }
+
+    public void disableCoralFeatures(boolean disable) {
+    }
+
+    public void disableAlgaeFeatures(boolean disable) {
+    }
+
+    public void disableParkFeatures(boolean disable) {
+    }
+
+    public void disableHpFeatures(boolean disable) {
+    }
+
+    public void disableDatabaseFeatures(boolean disable) {
+
+    }
+
+
+    public void disableInteraction(View element, boolean disable) {
+        element.setEnabled(!disable);
+        if (element instanceof CheckBox) {
+            ((CheckBox) element).setEnabled(!disable);
+        }
+        if (element instanceof RadioButton) {
+            ((RadioButton) element).setEnabled(!disable);
+        }
+    }
+
+    public void updateColorScheme() {
+        if (colorRadio == 0) {
+        } else {
+
+        }
     }
 }
