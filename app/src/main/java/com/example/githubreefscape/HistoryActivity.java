7850b40b83071860dd125b6d7b1378ac0aee2ac5
Kushal701151 <kshldurga@gmail.com>
2025-02-04 18:52:58 -0500

    Revert "dont use this"
    This reverts commit 168678ca3736ebf868553c6c237912f769d9e103.

--- app/src/main/java/com/example/githubreefscape/HistoryActivity.java
+++ app/src/main/java/com/example/githubreefscape/HistoryActivity.java
@@ -11,18 +11,12 @@ import android.widget.TextView;
 import android.view.View;
 import android.util.DisplayMetrics;
 import android.content.Intent;
-
-import java.lang.reflect.Type;
 import java.util.Dictionary;
 import android.content.SharedPreferences;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.json.JSONObject;
-import org.json.JSONObject;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 
 
@@ -33,7 +27,7 @@ public class HistoryActivity extends AppCompatActivity {
     public Button HSubmit;
 
     public String historystring;
-    public HashMap<String, String> myDictionary;
+
 
 
     @Override
@@ -48,17 +42,8 @@ public class HistoryActivity extends AppCompatActivity {
     }
     public void Submit(View view){
 
-        SharedPreferences sharedPreferences = getSharedPreferences("MyPreferences", MODE_PRIVATE);
 
-        // Get the JSON string from SharedPreferences
-        String json = sharedPreferences.getString(MainActivity.GlobalDictionary.MatchNumber.getText().toString(), "");
-
-        // Convert JSON string back to HashMap using Gson
-        Gson gson = new Gson();
-        Type type = new TypeToken<HashMap<String, String>>() {}.getType();
-        myDictionary = gson.fromJson(json, type);
-
-        historystring = myDictionary.get(Keyfinder.getText().toString());
+        historystring = MainActivity.GlobalDictionary.historydict.get(Keyfinder.getText().toString());
         String[] parts = historystring.split(",");
         // Convert the array into a List named historicallist
         ArrayList<String> historicallist = new ArrayList<>(Arrays.asList(parts));
